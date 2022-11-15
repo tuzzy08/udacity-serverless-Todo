2bd6635419335bb5abee4a1a35da2420e5554dad
Sage <tuzzy08@gmail.com>
2022-11-15 18:24:04 +0100

    feat: Added connection to serverless api

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,11 +1,11 @@
-// TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '...'
+//  Once your application is deployed, copy an API id here so that the frontend could interact with it
+const apiId = 'nf0npubzz6'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
-  // TODO: Create an Auth0 application and copy values from it into this map. For example:
+  // Create an Auth0 application and copy values from it into this map. For example:
   // domain: 'dev-nd9990-p4.us.auth0.com',
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'dev-ipd1t5ic12wexpbb.us.auth0.com', // Auth0 domain
+  clientId: 'Yobx3WPeXImiqr9ZLlRxV9oV1m1omCis', // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
