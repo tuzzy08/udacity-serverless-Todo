454f2243b5e7515c092a1f84548e0f7d380c460d
Sage <tuzzy08@gmail.com>
2022-11-15 17:29:36 +0100

    Fix: Fix merge conflicts

--- backend/src/dataLayer/attachmentUtils.ts
+++ backend/src/dataLayer/attachmentUtils.ts
@@ -6,32 +6,18 @@ const XAWS = AWSXRay.captureAWS(AWS)
 //  Implement the fileStorage logic
 const s3 = new XAWS.S3({ signatureVersion: 'v4' })
 const bucketName = process.env.ATTACHMENTS_S3_BUCKET
-<<<<<<< HEAD
 
 
 function getAttachmentUrl(todoID: string): string {
   const attachmentUrl = `https://${bucketName}.s3.amazonaws.com/${todoID}`
   return attachmentUrl
 }
-=======
-const urlExpiration = process.env.SIGNED_URL_EXPIRATION
-
-
-  function getAttachmentUrl(todoID: string): string {
-    const attachmentUrl = `https://${bucketName}.s3.amazonaws.com/${todoID}`
-    return attachmentUrl
-  }
->>>>>>> dd310d9d2d681a4b0b31ac4983028c55aefb9435
 
  async function getSignedUrl(todoID: string): Promise<string> {
    return s3.getSignedUrl('putObject', {
      Bucket: bucketName,
      Key: todoID,
-<<<<<<< HEAD
      Expires: 60000
-=======
-     Expires: urlExpiration
->>>>>>> dd310d9d2d681a4b0b31ac4983028c55aefb9435
    })
  }
   
